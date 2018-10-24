27cd0ab6db6114852dce07222c970d700a05b944
Jerry <m08632000@hotmail.com>
2018-10-23 22:45:52 -0700

    Updates

---
+++ precache-manifest.9dda42d34d5a20a47cc763a1899a1f6e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c9c21e82f7cca1404006ede8add5ecb",
+    "url": "/react_redux_transitionBackgroundGenerator/static/media/css.9c9c21e8.svg"
+  },
+  {
+    "revision": "bd1e26280f709e6e08c8",
+    "url": "/react_redux_transitionBackgroundGenerator/static/js/runtime~main.bd1e2628.js"
+  },
+  {
+    "revision": "f89f1d2853def5691ef4",
+    "url": "/react_redux_transitionBackgroundGenerator/static/js/main.f89f1d28.chunk.js"
+  },
+  {
+    "revision": "3550dde77df57b9f25d6",
+    "url": "/react_redux_transitionBackgroundGenerator/static/js/1.3550dde7.chunk.js"
+  },
+  {
+    "revision": "f89f1d2853def5691ef4",
+    "url": "/react_redux_transitionBackgroundGenerator/static/css/main.c1ee77ab.chunk.css"
+  },
+  {
+    "revision": "3550dde77df57b9f25d6",
+    "url": "/react_redux_transitionBackgroundGenerator/static/css/1.5ec22183.chunk.css"
+  },
+  {
+    "revision": "1880c3d52366d69c19f28fafe106540b",
+    "url": "/react_redux_transitionBackgroundGenerator/index.html"
+  }
+];
